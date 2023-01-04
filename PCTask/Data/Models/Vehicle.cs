b0b35f0551f0f5a1f31cd10be71adaa512992bfd
sippanas <alavicius.lukas@gmail.com>
2023-01-04 17:30:41 +0200

    Add solution files

---
+++ PCTask/Data/Models/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace PCTask.Data.Models
+{
+    public class Vehicle
+    { 
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int Year { get; set; }
+        public string? Engine { get; set; }
+    }
+}
