c7888be9b1805f1bda46746d315b217cb1293354
sippanas <alavicius.lukas@gmail.com>
2023-01-06 18:33:41 +0200

    Removed redundant pages & titles updates

--- PCTask/ClientApp/src/AppRoutes.js
+++ PCTask/ClientApp/src/AppRoutes.js
@@ -1,23 +1,8 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import VehiclesList from "./components/VehiclesList";
-import { Home } from "./components/Home";
 
 const AppRoutes = [
     {
         index: true,
-        element: <Home />
-    },
-    {
-        path: '/counter',
-        element: <Counter />
-    },
-    {
-        path: '/fetch-data',
-        element: <FetchData />
-    },
-    {
-        path: '/vehicles',
         element: <VehiclesList />
     }
 ];
