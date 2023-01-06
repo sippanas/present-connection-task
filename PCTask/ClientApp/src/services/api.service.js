80fbe4f6d6e9625433fbdae5e29b25c9e7a0fc81
sippanas <alavicius.lukas@gmail.com>
2023-01-06 17:42:54 +0200

    Add API service to frontend

---
+++ PCTask/ClientApp/src/services/api.service.js
@@ -0,0 +1,45 @@
+﻿import axios from 'axios';
+
+const getAllVehicles = async () => {
+    return await axios.get('api/vehicles')
+        .then((response) => {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch((e) => {
+            return e.response.status;
+        });
+};
+
+const getSpecificVehicle = async (id) => {
+    return await axios.get('api/vehicles/' + id)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch((e) => {
+            return e.response.status;
+        });
+};
+
+const createVehicle = async (make, model, year, engine) => {
+    return await axios.post('api/vehicles', { make, model, year, engine })
+        .then((response) => {
+            if (response.status === 201) {
+                return true;
+            }
+        })
+        .catch(() => {
+            return false;
+        });
+};
+
+const APIService = {
+    getAllVehicles,
+    getSpecificVehicle,
+    createVehicle
+};
+
+export default APIService;
