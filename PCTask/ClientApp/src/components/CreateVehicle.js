426cc400c9da3fa15e40f299d9ede48aab2cdc31
sippanas <alavicius.lukas@gmail.com>
2023-01-09 15:56:29 +0200

    Replaced year input field with dropdown

--- PCTask/ClientApp/src/components/CreateVehicle.js
+++ PCTask/ClientApp/src/components/CreateVehicle.js
@@ -1,18 +1,21 @@
 ﻿import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Spinner } from 'reactstrap';
+import { Spinner, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import validator from 'validator';
 import APIService from '../services/api.service';
 
 const CreateVehicle = () => {
+    const yearRangeMin = 1800;
+    const yearRangeMax = new Date().getFullYear() + 1;
     const navigate = useNavigate();
     const formValid = useRef(false);
-    const addingNew = useRef(false);
+    const [addingNew, setAddingNew] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const [formValues, setFormValues] = useState({
         make: '',
         model: '',
-        year: '',
+        year: 0,
         engine: ''
     });
 
@@ -26,7 +29,6 @@ const CreateVehicle = () => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        addingNew.current = true;
 
         APIService.createVehicle(formValues.make, formValues.model, formValues.year, formValues.engine)
             .then((created) => {
@@ -37,8 +39,6 @@ const CreateVehicle = () => {
                     alert('A server error has occured');
                 }
             });
-
-        addingNew.current = false;
     };
 
     const handleUserInput = (e) => {
@@ -62,9 +62,8 @@ const CreateVehicle = () => {
             }
             else errors.model = '';
 
-            if (validator.isEmpty(formValues.year) || !validator.isNumeric(formValues.year) ||
-                (formValues.year < 1500 || formValues.year > 9999)) {
-                errors.year = 'Field is invalid (Year should be between 1500-9999)';
+            if (!(formValues.year > 0)) {
+                errors.year = 'Field is required';
             }
             else errors.year = '';
 
@@ -76,7 +75,7 @@ const CreateVehicle = () => {
             setFormErrors({ ...errors });
 
             // If there are no validation errors in fields, set form as valid
-            formValid.current = validator.isEmpty(formErrors.make) && 
+            formValid.current = validator.isEmpty(formErrors.make) &&
                 validator.isEmpty(formErrors.model) &&
                 validator.isEmpty(formErrors.year) &&
                 validator.isEmpty(formErrors.engine)
@@ -84,12 +83,22 @@ const CreateVehicle = () => {
 
         validateForm();
 
-    }, [formValues]);
+    }, [formValues, formValues.year]);
+
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+    const onDropdownClicked = (value) => {
+        formValues.year = value;
+    };
+
+    // Used for creating year range in dropdown
+    const range = (start, stop, step) =>
+        Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 
     return (
         <div className="mx-auto px-5">
             <h1>Add new vehicle</h1>
-            <hr/>
+            <hr />
             <form onSubmit={onSubmit} noValidate>
                 <div className="mb-3 fw-bold">
                     <label htmlFor="makeInput" className="form-label">Make</label>
@@ -111,15 +120,17 @@ const CreateVehicle = () => {
                         {formErrors.model}
                     </div>
                 </div>
-                <div className="mb-3 fw-bold">
-                    <label htmlFor="yearInput" className="form-label">Year</label>
-                    <input name="year" type="text" id="yearInput" maxLength={4}
-                        className={`form-control ${formErrors.year.length > 0 ? 'is-invalid' : 'is-valid'}`}
-                        aria-describedby="yearInputFeedback"
-                        onChange={(e) => handleUserInput(e)} />
-                    <div id="yearInputFeedback" className="invalid-feedback">
-                        {formErrors.year}
-                    </div>
+                <div className="d-flex justify-content-start align-items-center gap-3 my-3">
+                    <div className="fw-bold">Year</div>
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle} direction="down">
+                        <DropdownToggle caret>{formValues.year > 0 ? formValues.year : 'Select year'}</DropdownToggle>
+                        <DropdownMenu className="dropdown-scroll">
+                            {range(yearRangeMin, yearRangeMax, 1).reverse().map((value) => {
+                                return <DropdownItem key={value} onClick={() => onDropdownClicked(value)}>{value}</DropdownItem>
+                            })}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <div className="fw-bold text-danger">{formErrors.year}</div>
                 </div>
                 <div className="mb-3 fw-bold">
                     <label htmlFor="engineInput" className="form-label">Engine</label>
@@ -132,9 +143,9 @@ const CreateVehicle = () => {
                     </div>
                 </div>
                 <button type="submit" className="btn btn-primary"
-                    disabled={!formValid.current}>
-                    {addingNew.current && <Spinner size="sm">Adding...</Spinner>}
-                    {!addingNew.current && <span>Add</span>}
+                    disabled={!formValid.current} onClick={() => setAddingNew(true)}>
+                    {addingNew && <Spinner size="sm">Adding...</Spinner>}
+                    {!addingNew && <span>{' '}Add</span>}
                 </button>
             </form>
         </div>
